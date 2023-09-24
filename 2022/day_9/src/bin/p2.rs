565767d748b2dbea10480f421d50410341727dd5
Sakib <sakibul706@gmail.com>
2023-09-24 07:01:14 +0600

    9 done

--- 2022/day_9/src/bin/p2.rs
+++ 2022/day_9/src/bin/p2.rs
@@ -1,11 +1,11 @@
 use std::fs;
 
-use day_9::p2;
+use day_9::part_2;
 
 fn main() {
     let intput = fs::read_to_string("input.txt").unwrap();
 
-    println!("p2 {:?}", p2(&intput));
+    println!("p2 {:?}", part_2(&intput));
 }
 
 
--- 2022/day_9/src/lib.rs
+++ 2022/day_9/src/lib.rs
@@ -1,15 +1,5 @@
 use std::collections::HashSet;
 
-// macro_rules! ternary {
-//     ($c:expr, $v:expr, $v1:expr) => {
-//         if $c {
-//             $v
-//         } else {
-//             $v1
-//         }
-//     };
-// }
-
 pub fn part_1(input: &str) -> usize {
     let mut set: HashSet<(i32, i32)> = HashSet::new();
     set.insert((0, 0));
@@ -56,6 +46,62 @@ pub fn part_1(input: &str) -> usize {
                 }
             }
             set.insert(tail);
+        }
+    }
+
+    set.len()
+}
+
+pub fn part_2(input: &str) -> usize {
+    let mut set: HashSet<(i32, i32)> = HashSet::new();
+    set.insert((0, 0));
+    let mut rope = vec![(0, 0); 10];
+
+    for line in input.trim().lines() {
+        let arg = line.split(" ").collect::<Vec<_>>();
+        let cmd = arg[0];
+        let mv = arg[1]
+            .parse::<i32>()
+            .expect("part_1: while parsing argumnet");
+        for _ in 0..mv {
+            let head = &mut rope[0];
+            match cmd {
+                "R" => {
+                    head.0 += 1;
+                }
+                "L" => {
+                    head.0 -= 1;
+                }
+                "U" => {
+                    head.1 += 1;
+                }
+                "D" => {
+                    head.1 -= 1;
+                }
+                _ => panic!("unexpected instruction {cmd}"),
+            }
+
+            // for i in
+            for i in 0..rope.len() - 1 {
+                let head = &rope[i];
+                let tail = &rope[i + 1];
+                let dx: i32 = head.0 - tail.0;
+                let dy: i32 = head.1 - tail.1;
+
+                if dx.abs() > 1 || dy.abs() > 1 {
+                    // let _x = ternary![dx > 0, 1, -1];
+                    // let _y = ternary![dy > 0, 1, -1];
+                    if dx == 0 {
+                        rope[i + 1].1 += dy / 2;
+                    } else if dy == 0 {
+                        rope[i + 1].0 += dx / 2;
+                    } else {
+                        rope[i + 1].0 += if dx > 0 { 1 } else { -1 };
+                        rope[i + 1].1 += if dy > 0 { 1 } else { -1 };
+                    }
+                }
+            }
+            set.insert(rope.last().unwrap().clone());
         }
     }
 
