f6f56b97c5a959b26637032c0a51407bec3cc400
Sakib <sakibul706@gmail.com>
2023-09-25 18:02:47 +0600

    11 prog

---
+++ 2022/day_11/src/lib.rs
@@ -0,0 +1,80 @@
+#[allow(unused)]
+pub mod mnk;
+pub mod part_1;
+pub mod part_2;
+
+#[derive(Debug)]
+enum Operator {
+    None,
+    Plus,
+    Multiply,
+}
+
+#[allow(unused)]
+#[derive(Debug)]
+struct Monkey {
+    idx: usize,
+    op: Operation,
+    items: Vec<usize>,
+    test: Test,
+}
+
+#[derive(Debug)]
+struct Operation {
+    operatin: Operator,
+    number: Option<usize>,
+}
+
+#[derive(Debug)]
+struct Test {
+    devide_by: usize,
+    if_true: usize,
+    if_false: usize,
+}
+
+impl Monkey {
+    fn new(idx: usize) -> Monkey {
+        Monkey {
+            idx,
+            items: vec![],
+            op: Operation {
+                operatin: Operator::None,
+                number: None,
+            },
+            test: Test {
+                devide_by: 0,
+                if_true: 0,
+                if_false: 0,
+            },
+        }
+    }
+}
+
+const _TEST_STR: &str = "Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+";
---
+++ 2022/day_11/src/mnk.rs
@@ -0,0 +1,92 @@
+use crate::*;
+
+fn p1(input: &str) -> Vec<Monkey> {
+    let mut game: Vec<Monkey> = Vec::new();
+
+    for (i, round) in input.trim().split("\n\n").enumerate() {
+        let mut mnky = Monkey::new(i);
+        let mut gm = round.trim().lines();
+        // skip monky idx
+        gm.next().unwrap();
+
+        // starting items
+        mnky.items = gm
+            .next()
+            .unwrap()
+            .split(":")
+            .last()
+            .unwrap()
+            .trim()
+            .split(",")
+            .map(|itm| itm.trim().parse::<usize>().unwrap())
+            .collect::<Vec<_>>();
+
+        // operatins
+        let op = gm.next().unwrap().trim();
+        let operator = op
+            .split("=")
+            .last()
+            .unwrap()
+            .trim()
+            .split_whitespace()
+            .nth(1)
+            .unwrap();
+
+        match operator {
+            "+" => mnky.op.operatin = Operator::Plus,
+            "*" => mnky.op.operatin = Operator::Multiply,
+            _ => panic!("unknown '{operator}' operator"),
+        }
+
+        // op.split(" ").last().unwrap();
+        mnky.op.number = match op.split_whitespace().last().unwrap().parse::<usize>() {
+            Ok(val) => Some(val),
+            Err(_) => None,
+        };
+
+        // test
+        mnky.test.devide_by = gm
+            .next()
+            .unwrap()
+            .trim()
+            .split_whitespace()
+            .last()
+            .unwrap()
+            .parse::<usize>()
+            .unwrap();
+
+        mnky.test.if_true = gm
+            .next()
+            .unwrap()
+            .trim()
+            .split_whitespace()
+            .last()
+            .unwrap()
+            .parse::<usize>()
+            .unwrap();
+
+        mnky.test.if_false = gm
+            .next()
+            .unwrap()
+            .trim()
+            .split_whitespace()
+            .last()
+            .unwrap()
+            .parse::<usize>()
+            .unwrap();
+        game.push(mnky);
+    }
+
+    game
+}
+
+// mod tests {
+//     #[test]
+//     fn test_p1() {
+//         use super::*;
+//         use crate::_TEST_STR;
+//
+//         p1(_TEST_STR);
+//         assert!(false);
+//     }
+// }
